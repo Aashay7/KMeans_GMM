e0f73fb81308c7c26c86cbaa9a746c92b6173717
Aashay Gondalia <aagond@iu.edu>
2022-03-29 16:41:02 -0400

    minor updates

--- gmm.py
+++ gmm.py
@@ -2,68 +2,95 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class GaussianMixture:
-    def __init__(self, num_centroids, data):
+    def __init__(self, num_gaussians, data):
         self.X                  = np.array(data)
-        self.k                  = num_centroids
-        self.centroid_mean      = {}
-        self.centroid_variance  = {}
-        self.centroid_scaling   = {}
-        self.centroid_members   = {}
+        self.k                  = num_gaussians
+        self.gaussian_mean      = {}
+        self.gaussian_variance  = {}
+        self.gaussian_scaling   = {}
+        self.gaussian_members   = {}
         self.likelihood         = []
         self.posterior          = {}
+        self.history            = {}
         self.delta              = 1e-10
-        self.initialize_centroids()
+        self.initialize_gaussians()
 
 
-    def initialize_centroids(self):
-        weights     = np.ones(shape=(self.k)) / self.k
-        means       = np.linspace(np.min(self.X), np.max(self.X), self.k)
-        variance    = weights * np.var(self.X)
+    def initialize_gaussians(self):
+        scaling     = np.ones(shape=(self.k)) / self.k
+        means       = np.linspace(np.min(self.X), np.max(self.X), self.k) 
+        variance    = scaling * np.var(self.X)
         
+        self.history['mean']        = {}
+        self.history['variance']    = {}
+        self.history['scaling']     = {}
+
         for i in range(self.k):
-            self.centroid_mean[f"centroid_{i}"]     = means[i]
-            self.centroid_variance[f"centroid_{i}"] = variance[i]
-            self.centroid_scaling[f"centroid_{i}"]  = weights[i]
-            self.history_mean[f"centroid_{i}"]      = [means[i]]
+            self.gaussian_mean[f"gaussian_{i}"]         = means[i]
+            self.gaussian_variance[f"gaussian_{i}"]     = variance[i]
+            self.gaussian_scaling[f"gaussian_{i}"]      = scaling[i]
+            self.history['mean'][f"gaussian_{i}"]       = [means[i]]
+            self.history['variance'][f"gaussian_{i}"]   = [variance[i]]
+            self.history['scaling'][f"gaussian_{i}"]    = [scaling[i]]
 
-        print(f"Initialized centroids mean:     {self.centroid_mean}")
-        print(f"Initialized centroids variance: {self.centroid_variance}")
-        print(f"Initialized centroids scaling:  {self.centroid_scaling}")
+        print(f"Initialized gaussians mean:     {self.gaussian_mean}")
+        print(f"Initialized gaussians variance: {self.gaussian_variance}")
+        print(f"Initialized gaussians scaling:  {self.gaussian_scaling}")
 
 
-    def get_gaussian_likelihood(self, centroid):
-        normalize_term  = 1 / (np.sqrt(2 * np.pi * (self.centroid_variance[centroid])))
-        exp_term        = np.exp(-(np.power((self.X - self.centroid_mean[centroid]),2)) / (2*self.centroid_variance[centroid])) 
+    def get_gaussian_likelihood(self, data, mean, variance):
+        """
+        Calculates the likelihood of the data exemplars to fall under a given 
+        gaussian gaussian with unique mean, variance and scaling.
+
+        Args:
+            gaussian (string): Key/id of the gaussian.
+
+        Returns:
+            likelihood (float) : The likelihood value of each exemplar to be associated with the given gaussian.
+        """
+        normalize_term  = 1 / (np.sqrt(2 * np.pi * (variance)))
+        exp_term        = np.exp(-(np.power((data - mean),2)) / (2*variance)) 
         return normalize_term * exp_term
 
 
     def E_step(self):
         """
-        
+        The E-step or Expectation step consists of calculating the likelihood
+        of each x_i by using the estimated parameters.
+        The function calculates the likelihood of a given example x_i to belong
+        to a given cluster.
         """
-        # Calculate the likelihood of each observation x_i using the 
-        # estimated parameters.
         self.likelihood = []
-        for centroid in self.centroid_mean.keys():
-            likely = self.get_gaussian_likelihood(centroid)
+        for gaussian in self.gaussian_mean.keys():
+            likely = self.get_gaussian_likelihood(self.X, self.gaussian_mean[gaussian], self.gaussian_variance[gaussian])
             self.likelihood.append(likely)
         self.likelihood= np.array(self.likelihood)
         
 
     def M_step(self):
         # Recompute mean, variance and scaling factor.
-        for j, centroid in enumerate(self.centroid_mean.keys()):
-            self.posterior[centroid] = []
-            posterior_val = (self.likelihood[j] * self.centroid_scaling[centroid]) / (np.sum([self.likelihood[i] * self.centroid_scaling[ctr] for i,ctr in enumerate(self.centroid_mean.keys())], axis=0) + self.delta)
-            self.posterior[centroid].append(posterior_val)
+        """
+        The M-Step or Maximization step consists of updating the parameters of 
+        the model such as the cluster/gaussian mean, variance and the scaling factor. 
+        """
+        for j, gaussian in enumerate(self.gaussian_mean.keys()):
+            self.posterior[gaussian] = []
+            posterior_val = (self.likelihood[j] * self.gaussian_scaling[gaussian]) / (np.sum([self.likelihood[i] * self.gaussian_scaling[ctr] for i,ctr in enumerate(self.gaussian_mean.keys())], axis=0) + self.delta)
+            self.posterior[gaussian].append(posterior_val)
 
-            self.centroid_mean[centroid]        = np.sum(self.posterior[centroid] * self.X) / (np.sum(self.posterior[centroid]) + self.delta)
-            self.centroid_variance[centroid]    = np.sum(self.posterior[centroid] * np.square(self.X- self.centroid_mean[centroid])) / (np.sum(self.posterior[centroid]) + self.delta)
-            self.centroid_scaling[centroid]     = np.mean(self.posterior[centroid])
+            self.gaussian_mean[gaussian]        = np.sum(self.posterior[gaussian] * self.X) / (np.sum(self.posterior[gaussian]) + self.delta)
+            self.gaussian_variance[gaussian]    = np.sum(self.posterior[gaussian] * np.square(self.X- self.gaussian_mean[gaussian])) / (np.sum(self.posterior[gaussian]) + self.delta)
+            self.gaussian_scaling[gaussian]     = np.mean(self.posterior[gaussian])
+
+            self.history['mean'][gaussian].append(self.gaussian_mean[gaussian])
+            self.history['variance'][gaussian].append(self.gaussian_variance[gaussian])
+            self.history['scaling'][gaussian].append(self.gaussian_scaling[gaussian])
             
-        print(f"The Centroid Mean is        {self.centroid_mean}")
-        print(f"The Centroid Variance is    {self.centroid_variance}")
-        print(f"The Centroid Scaling is     {self.centroid_scaling}\n\n")
+            
+        print(f"The Gaussian Mean is        {self.gaussian_mean}")
+        print(f"The Gaussian Variance is    {self.gaussian_variance}")
+        print(f"The Gaussian Scaling is     {self.gaussian_scaling}\n\n")
 
 
     def learn(self, cycles=10):
@@ -73,10 +100,24 @@ class GaussianMixture:
             self.M_step()
 
 
-    def plot_clusters(self):
-        plt.figure(figsize=(20,10))
-        plt.hist(self.X, bins=50)
-        for centroid, centroid_x in self.centroids.items():
-            plt.plot(centroid_x, 0, 'o', alpha = 0.5, markersize=20*(15-len(self.centroids)))
-            plt.plot(centroid_x, 10*(15-len(self.centroids))//8, 'o:k', markersize=20)
+    def plot_evolution(self):
+        bins = np.linspace(np.min(self.X), np.max(self.X), 100)
+        plt.figure(figsize=(30,15))
+        plt.xlabel("x")
+        plt.ylabel("pdf")
+        plt.scatter(self.X, [0.]*len(self.X), color='red', marker='+', label='train data')
+        #plt.hist(self.X, bins=50)
+        
+        for gaussian, gaussian_mean_list in self.history["mean"].items():
+            color = (np.random.random(), np.random.random(), np.random.random())
+            for i,gaussian_mean in enumerate(gaussian_mean_list):
+                alpha_val = 0.01 + i/(len(gaussian_mean_list) + 10)
+                plt.plot(bins, self.get_gaussian_likelihood(bins, self.history["mean"][gaussian][i],  self.history["variance"][gaussian][i]), color=color, label="Cluster 1", alpha = alpha_val)
         return plt
+
+"""
+We see that there are far fewer blue stars (Cluster 0) than compared to the red stars (Cluster 1).
+The stars we see in our galaxy are far lesser than the stars from the other galaxy. 
+This is logical as a image is a 2D representation of the 3D space, as the distance increases, the scope window also increases and as a result more stars from far away are captured (however they might be a bit dimmer!)
+
+"""
--- kmeans.py
+++ kmeans.py
@@ -2,61 +2,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class KMeans:
-    def __init__(self, num_centroids, data):
+    def __init__(self, num_clusters, data):
         self.X = data
-        self.k = num_centroids
-        self.centroids = {}
-        self.centroid_members = {}
+        self.k = num_clusters
+        self.clusters = {}
+        self.cluster_members = {}
         self.history = {}
-        self.initialize_centroids()
+        self.initialize_clusters()
 
 
-    def initialize_centroids(self):
+    def initialize_clusters(self):
         for i in range(self.k):
-            self.centroids[f"centroid_{i}"] = np.random.choice(self.X)
-            self.history[f"centroid_{i}"] = [self.centroids[f"centroid_{i}"]]
-            self.centroid_members[f"centroid_{i}"] = [0]*len(self.X)
-        print(f"Initialized centroids : {self.centroids}")
+            self.clusters[f"cluster_{i}"] = np.random.choice(self.X)
+            self.history[f"cluster_{i}"] = [self.clusters[f"cluster_{i}"]]
+            self.cluster_members[f"cluster_{i}"] = [0]*len(self.X)
+        print(f"Initialized clusters : {self.clusters}")
     
 
-    def initialize_centroid_members(self):
+    def initialize_cluster_members(self):
         for i in range(self.k):
-            self.centroid_members[f"centroid_{i}"] = [0]*len(self.X)
+            self.cluster_members[f"cluster_{i}"] = [0]*len(self.X)
 
 
-    def form_clusters(self,  cycles=10, threshold=0.1):
+    def form_clusters(self,  cycles=10):
+        """
+        The form_clusters method performs the cluster assignment step and 
+        subsequently the mean update step. 
+
+        Args:
+            cycles (int, optional): Number of cycles/epochs. Defaults to 10.
+        """
         for i in range(cycles):
             print(f"Iteration : {i+1}")
-            # Assignment
-            self.initialize_centroid_members()
+            # Cluster Assignment step
+            self.initialize_cluster_members()
             for j, datum_x in enumerate(self.X):
                 min_dist, min_index = 1e40, None
                 
-                for centroid, centroid_x in self.centroids.items():
-                    euclidean_dist = (datum_x - centroid_x)**2
+                for cluster, cluster_x in self.clusters.items():
+                    euclidean_dist = (datum_x - cluster_x)**2
                     if euclidean_dist < min_dist:
                         min_dist = euclidean_dist
-                        min_index= centroid
-                self.centroid_members[min_index][j] = 1
-            print(f"Assigned members to centroids : {self.centroid_members}")
+                        min_index= cluster
+                self.cluster_members[min_index][j] = 1
+            print(f"Assigned members to clusters : {self.cluster_members}")
             
-            # Update the means
-            for centroid, centroid_member in self.centroid_members.items():
-                print(f"{centroid} : {centroid_member}")
-                self.centroids[centroid] =  np.sum(np.multiply(np.array(self.centroid_members[centroid]), np.array(self.X))) / np.sum(np.array(self.centroid_members[centroid])>0)
-                self.history[centroid].append(self.centroids[centroid])
-            print(f"Updated centroids : {self.centroids}\n")
+            # Mean Update step
+            for cluster, cluster_member in self.cluster_members.items():
+                print(f"{cluster} : {cluster_member}")
+                self.clusters[cluster] =  np.sum(np.multiply(np.array(self.cluster_members[cluster]), np.array(self.X))) / np.sum(np.array(self.cluster_members[cluster])>0)
+                self.history[cluster].append(self.clusters[cluster])
+            print(f"Updated clusters : {self.clusters}\n")
             
 
-    def get_centroid_value(self):
-        for centroid, centroid_x in self.centroids.items():
-            print(f"{centroid} : {centroid_x}")
+    def get_cluster_centroids(self):
+        for cluster, cluster_x in self.clusters.items():
+            print(f"{cluster} : {cluster_x}")
 
 
     def plot_clusters(self):
         plt.figure(figsize=(20,10))
         plt.hist(self.X, bins=50)
-        for centroid, centroid_x in self.centroids.items():
-            plt.plot(centroid_x, 0, 'o', alpha = 0.5, markersize=20*(15-len(self.centroids)))
-            plt.plot(centroid_x, 10*(15-len(self.centroids))//8, 'o:k', markersize=20)
+        for cluster, cluster_x in self.clusters.items():
+            plt.plot(cluster_x, 0, 'o', alpha = 0.5, markersize=20*(15-len(self.clusters)))
+            plt.plot(cluster_x, 10*(15-len(self.clusters))//8, 'o:k', markersize=20)
         return plt
